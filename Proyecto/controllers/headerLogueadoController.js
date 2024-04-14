67509a8e7733a69abc0974e9f41e1c1b4f331951
ninaellenberg <nellenberg@udesa.edu.ar>
2024-04-14 19:54:38 -0300

    Creacion de formularios y agregue el modulo de usuarios a la vista headerLogueado

--- Proyecto/controllers/headerLogueadoController.js
+++ Proyecto/controllers/headerLogueadoController.js
@@ -1,4 +1,12 @@
-const headerLogueadoController= {
-
-}
-module.exports = headerLogueadoController
+const headerLogueadoController = {
+    index: function(req, res, next) {
+      // ahora quieor que obtenga el nombre de usuario del modulo de datos
+      const usuario = db.usuario.usuario;
+  
+      // ahora q me renderice la vista 'profileEdit' y pasar el nombre de usuario
+      res.render('profileEdit', { usuario: usuario });
+    }
+  };
+  
+  module.exports = headerLogueadoController;
+  
