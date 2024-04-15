ef9ba36e64646f13e2f9574e542f4b13c36a3402
Valen Lambert <vlambert@udesa.edu.ar>
2024-04-15 14:37:17 -0300

    busqueda

--- Proyecto/routes/index.js
+++ Proyecto/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get ("/buscar", function(req, res, next) {
-  let buscar = req.query.buscar;
+  let buscar = req.query.search;
   res.send (buscar);
 });
 
